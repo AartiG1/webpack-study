bddfc882d0cc7e658e8989844a78643fefc7bf1e
Basavaraj Sonnad <basavaraj.sonnad@agrostar.in>
2018-03-15 09:46:30 +0530

    list api exposed

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-import getName from "./app";
+import {createList} from "./list";
 
-console.log(getName())
+var list = createList();
+
+list.add(2); // 2
+
+list.add([4, 5,6]);
+list.print(); // 2,4,5,6
