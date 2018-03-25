d2dc361447c1bbc5119ba174291cb9c40c1cb051
Basavaraj Sonnad <basavaraj.sonnad@agrostar.in>
2018-03-25 11:57:45 +0530

    assignment added

--- index.js
+++ index.js
@@ -1,20 +1,58 @@
 // ES 6 modules
-import {createList} from "./list";
+import MyList, { createList, MyListES6} from "./list";
 
 // nodejs -common js
 // var createList =  require("./list").createList;
 
 
 var list = createList();
-list.add(2); // 2
-list.add([3,4,5]);
-list.add({
-    a: 4,
-    b: 4
-});
+list.add(2);
+// ---------------------- ES6 Tests --------
+
+
+const listes6 = new MyListES6();
+
+listes6.add(3);
+listes6.add(4);
+listes6.print();
+
+
+
+
+// 2
+// list.add([3,4,5]);
+// list.add({
+//     a: 4,
+//     b: 4
+// });
 
 list.print(); // 2,4,5,6
 
 // new list
-var list2 = createList();
-console.log("pune", list2)
+// var list2 = createList();
+// console.log("pune", list2)
+
+// ------ Array of person /////
+
+const peopleList = new MyListES6();
+
+peopleList.add({
+    name: "pune",
+    age: 32,
+    rollNo: 22
+});
+
+peopleList.add({
+    name: "university",
+    age: 4,
+    rollNo: 24
+});
+
+peopleList.pluck('name') // -> ["pune", "university"] 
+peopleList.reject(function() {
+    return this.rollNo > 22;
+})
+// ---> [{
+// name: "pune",
+//     rollNo: 22
+// }]
--- list.js
+++ list.js
@@ -3,8 +3,34 @@ export function createList() {
     return new MyList();
 }
 
-// constructor fn
-function MyList() {
+export class MyListES6 {
+    constructor() {
+        this.arr = []; 
+    }
+
+    add(val) {
+        this.arr.push(val);
+    }
+
+    print() {
+        console.log(this.arr);
+    }
+
+    flatten() {
+        // this.arr
+    }
+    uniq() {
+        // 
+        // this.arr
+    }
+
+    pluck(key) {}
+
+    reject(predicateFn) {}
+}
+
+// constructor fn and default export
+export default function MyList() {
     this.arr = [];
 }
 
@@ -27,3 +53,11 @@ MyList.prototype.head = function () {
 // 2. tail fn -> return last element -> -1
 // 3. searchFromEnd fn-> index of element from the end of arr
 // 4. add fn -> no param -> fail safe
+//  --------- new assignment with array of objects-----------
+
+// 6. reject - > reject(predicateFn,) 
+// 7. pluck(propertyName) 
+
+//  --------- new assignment with array of primitives-----------
+// 8 uniq([1, 2, 1, 4, 1, 3]); --> [1, 2, 4, 3]
+// 9. flatten([1, [2], [3, [[4]]]]); --> [1, 2, 3, 4]
